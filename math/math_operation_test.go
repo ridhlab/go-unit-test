3777c0ed232fdba99f252bc863013d0909088e41
Muhammad Ridwan <mohridwan1707@gmail.com>
2023-04-06 20:50:47 +0700

    add assertion

--- math/math_operation_test.go
+++ math/math_operation_test.go
@@ -1,32 +1,55 @@
 package mathOperation
 
-import "testing"
+import (
+	"testing"
+
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Test started")
+	m.Run()
+	fmt.Println("Test finished")
+}
 
 func TestSum(t *testing.T) {
-	res := Sum(1, 2)
-	if res != 3 {
-		t.Error("res must be 3")
-	}
+	assert.Equal(t, 4, Sum(2, 2), "Sum result must be equal")
+	assert.Equal(t, 22, Sum(10, 12), "Sum result must be equal")
+	assert.Equal(t, 27, Sum(13, 14), "Sum result must be equal")
+
+	assert.NotEqual(t, 5, Sum(2, 2), "Sum result must not equal")
+	assert.NotEqual(t, 11, Sum(10, 2), "Sum result must not equal")
+	assert.NotEqual(t, 9, Sum(8, 2), "Sum result must not equal")
 }
 
 func TestSubstraction(t *testing.T) {
-	res := Substraction(1, 2)
-	if res != -1 {
-		t.Error("res mus be -1")
-	}
+	assert.Equal(t, 0, Substraction(2, 2), "Substraction result must be equal")
+	assert.Equal(t, -2, Substraction(10, 12), "Substraction result must be equal")
+	assert.Equal(t, -1, Substraction(13, 14), "Substraction result must be equal")
+
+	assert.NotEqual(t, 1, Substraction(2, 2), "Substraction result must not equal")
+	assert.NotEqual(t, 11, Substraction(10, 2), "Substraction result must not equal")
+	assert.NotEqual(t, 9, Substraction(8, 2), "Substraction result must not equal")
 }
 
 func TestMultiple(t *testing.T) {
-	res := Multiple(1, 2)
-	if res != 2 {
-		panic("Test is not valid")
-	}
+	assert.Equal(t, 4, Multiple(2, 2), "Multiple result must be equal")
+	assert.Equal(t, 120, Multiple(10, 12), "Multiple result must be equal")
+	assert.Equal(t, 182, Multiple(13, 14), "Multiple result must be equal")
+
+	assert.NotEqual(t, 5, Multiple(2, 2), "Multiple result must not equal")
+	assert.NotEqual(t, 11, Multiple(10, 2), "Multiple result must not equal")
+	assert.NotEqual(t, 9, Multiple(8, 2), "Multiple result must not equal")
 }
 
 func TestDivide(t *testing.T) {
-	res1 := Divide(1, 2)
-	res2 := Divide(2, 2)
-	if res1 != 0 || res2 != 1 {
-		panic("Test is not valid")
-	}
+	assert.Equal(t, 1, Divide(2, 2), "Divide result must be equal")
+	assert.Equal(t, 4, Divide(4, 1), "Divide result must be equal")
+	assert.Equal(t, 3, Divide(40, 12), "Divide result must be equal")
+
+	assert.NotEqual(t, 5, Divide(2, 2), "Divide result must not equal")
+	assert.NotEqual(t, 11, Divide(10, 2), "Divide result must not equal")
+	assert.NotEqual(t, 9, Divide(8, 2), "Divide result must not equal")
 }
